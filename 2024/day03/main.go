69bc5d031d427605c7957272ec5110eec61e621e
Wivianne Grapenholt <wivianne.grapenholt@minso.com>
2026-01-01 20:25:57 +0100

    wip: 2024 - day 3

---
+++ 2024/day03/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"adventofcode2025/utils"
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+func GetTestInput() string {
+	return "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+}
+
+func GetInput() string {
+	lines, err := utils.ReadInputAsString("day03")
+	if err != nil {
+		panic(err)
+	}
+
+	return lines
+}
+
+func main() {
+	// line:=GetInput()
+	line := GetTestInput()
+
+	fmt.Println("Day 03, Part 1 Answer: ", partOne(line))
+	fmt.Println("Day 03, Part 2 Answer: ", partTwo(line))
+}
+
+func partOne(line string) int {
+	total := 0
+	re := regexp.MustCompile(`(?i)mul\([0-9]{1,3},[0-9]{1,3}\)`)
+	bites := re.FindAll([]byte(line), -1)
+
+	validMemory := []string{}
+	for _, byteSlice := range bites {
+		validMemory = append(validMemory, string(byteSlice))
+	}
+
+	for _, raw := range validMemory {
+		valid := strings.Trim(raw, "mul()")
+		numbers := utils.StringToIntArr(strings.Split(valid, ","))
+
+		total += numbers[0] * numbers[1]
+	}
+	return total
+}
+
+func partTwo(line string) int {
+	total := len(line)
+	// get index for all do() and don't()
+	// only mul(X,Y) after do() befor don't() are valid...
+	// starting with don't()
+
+	return total
+}
---
+++ 2024/day03/main_test.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/matryer/is"
+)
+
+func Test_partOne(t *testing.T) {
+	tests := []struct {
+		name string
+		line string
+		want int
+	}{
+		{
+			name: "happy test input",
+			line: GetTestInput(),
+			want: 161,
+		},
+		{
+			name: "Happy Path",
+			line: GetInput(),
+			want: 164730528,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			is := is.New(t)
+			got := partOne(tt.line)
+			is.Equal(got, tt.want)
+		})
+	}
+}
+
+func Test_partTwo(t *testing.T) {
+	tests := []struct {
+		name  string
+		lines string
+		want  int
+	}{
+		// FIXME: test input should be: 48
+		{
+			name:  "check test input length",
+			lines: GetTestInput(),
+			want:  71,
+		},
+		{
+			name:  "check length",
+			lines: GetInput(),
+			want:  17699,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			is := is.New(t)
+			got := partTwo(tt.lines)
+			is.Equal(got, tt.want)
+		})
+	}
+}
