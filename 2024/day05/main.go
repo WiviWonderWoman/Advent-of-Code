e254c22ec0d997cef7ee3d2b7593c36f2d3d84c5
Wivianne Grapenholt <wivianne.grapenholt@minso.com>
2026-01-03 12:25:03 +0100

    feat: 2024 - day 5 part 1

---
+++ 2024/day05/main.go
@@ -0,0 +1,147 @@
+package main
+
+import (
+	"adventofcode2025/utils"
+	"fmt"
+	"slices"
+	"strings"
+)
+
+func GetTestInput() []string {
+	return []string{
+		"47|53",
+		"97|13",
+		"97|61",
+		"97|47",
+		"75|29",
+		"61|13",
+		"75|53",
+		"29|13",
+		"97|29",
+		"53|29",
+		"61|53",
+		"97|53",
+		"61|29",
+		"47|13",
+		"75|47",
+		"97|75",
+		"47|61",
+		"75|61",
+		"47|29",
+		"75|13",
+		"53|13",
+		"",
+		"75,47,61,53,29",
+		"97,61,53,29,13",
+		"75,29,13",
+		"75,97,47,61,53",
+		"61,13,29",
+		"97,13,75,29,47",
+	}
+}
+
+func GetInput() []string {
+	lines, err := utils.ReadInput("day05")
+	if err != nil {
+		panic(err)
+	}
+
+	return lines
+}
+
+func main() {
+	// lines:=GetInput()
+	lines := GetTestInput()
+
+	fmt.Println("Day 05, Part 1 Answer: ", partOne(lines))
+	fmt.Println("Day 05, Part 2 Answer: ", partTwo(lines))
+}
+
+func partOne(lines []string) int {
+	total := 0
+
+	idx := slices.Index(lines, "")
+	rules := lines[0:idx]
+	updates := lines[idx:]
+
+	// find valid updates
+	valid := checkUpadates(updates, rules)
+
+	// add middle page-numbers together
+	for _, update := range valid {
+		pages := strings.Split(update, ",")
+		length := len(pages)
+
+		middle := pages[length/2]
+		if middle != "" {
+			total += utils.StringToInt(middle)
+		}
+
+	}
+
+	return total
+}
+
+func checkUpadates(updates, rules []string) []string {
+	valid := []string{}
+
+	for _, update := range updates {
+		pages := strings.Split(update, ",")
+		ok := true
+		current := ""
+		var before []string
+		var after []string
+
+		for i := range pages {
+			current = pages[i]
+			if current == "" {
+				continue
+			}
+
+			before = pages[:i]
+			after = pages[i+1:]
+
+			ok = checkRules(rules, current, before, after)
+			if !ok {
+				break
+			}
+		}
+
+		if !ok {
+			continue
+		}
+
+		valid = append(valid, update)
+	}
+
+	return valid
+}
+
+func checkRules(rules []string, page string, before, after []string) bool {
+	for _, rule := range rules {
+		ok := true
+		current := strings.Split(rule, "|")
+
+		if strings.HasPrefix(rule, page) && len(before) > 0 {
+			if slices.Contains(before, current[1]) {
+				ok = false
+			}
+		} else if strings.HasSuffix(rule, page) && len(after) > 0 {
+			if slices.Contains(after, current[0]) {
+				ok = false
+			}
+		}
+
+		if !ok {
+			return false
+		}
+	}
+
+	return true
+}
+
+func partTwo(lines []string) int {
+	total := len(lines)
+
+	return total
+}
---
+++ 2024/day05/main_test.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/matryer/is"
+)
+
+func Test_partOne(t *testing.T) {
+	tests := []struct {
+		name  string
+		lines []string
+		want  int
+	}{
+		{
+			name:  "happy test input",
+			lines: GetTestInput(),
+			want:  143,
+		},
+		{
+			name:  "Happy Path",
+			lines: GetInput(),
+			want:  3608,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			is := is.New(t)
+			got := partOne(tt.lines)
+			is.Equal(got, tt.want)
+		})
+	}
+}
+
+func Test_partTwo(t *testing.T) {
+	tests := []struct {
+		name  string
+		lines []string
+		want  int
+	}{
+		{
+			name:  "check test input length",
+			lines: GetTestInput(),
+			want:  28,
+		},
+		{
+			name:  "check length",
+			lines: GetInput(),
+			want:  1351,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			is := is.New(t)
+			got := partTwo(tt.lines)
+			is.Equal(got, tt.want)
+		})
+	}
+}
