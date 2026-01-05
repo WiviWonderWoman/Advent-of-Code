26b51a5fc3e58478b7a7196529885de9b9c8ac06
Wivianne Grapenholt <wivianne.grapenholt@minso.com>
2026-01-05 23:23:21 +0100

    wip: day 8

---
+++ 2024/day08/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"adventofcode2025/utils"
+	"fmt"
+)
+
+func GetTestInput() []string {
+	return []string{
+		"............",
+		"........0...",
+		".....0......",
+		".......0....",
+		"....0.......",
+		"......A.....",
+		"............",
+		"............",
+		"........A...",
+		".........A..",
+		"............",
+		"............",
+	}
+}
+
+func GetInput() []string {
+	lines, err := utils.ReadInput("day08")
+	if err != nil {
+		panic(err)
+	}
+
+	return lines
+}
+
+func main() {
+	// lines:=GetInput()
+	lines := GetTestInput()
+
+	fmt.Println("Day 08, Part 1 Answer: ", partOne(lines))
+	fmt.Println("Day 08, Part 2 Answer: ", partTwo(lines))
+}
+
+type Position struct {
+	row int
+	col int
+}
+
+func partOne(lines []string) int {
+	total := len(lines)
+
+	return total
+}
+
+func partTwo(lines []string) int {
+	total := len(lines)
+
+	return total
+}
---
+++ 2024/day08/main_test.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"slices"
+	"testing"
+
+	"github.com/matryer/is"
+)
+
+func Test_partOne(t *testing.T) {
+	tests := []struct {
+		name  string
+		lines []string
+		wrong []int
+		want  int
+	}{
+		{
+			name:  "check test input length",
+			lines: GetTestInput(),
+			want:  12,
+		},
+		// {
+		// 	name:  "Too high",
+		// 	lines: GetInput(),
+		// 	wrong: []int{219,386},
+		// },
+		{
+			name:  "check length",
+			lines: GetInput(),
+			want:  50,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			is := is.New(t)
+			got := partOne(tt.lines)
+			if len(tt.wrong) > 0 {
+				if tt.name == "Too low" {
+					for _, w := range tt.wrong {
+						is.Equal(slices.Compare([]int{got}, []int{w}), 1)
+					}
+				}
+
+				if tt.name == "Too high" {
+					for _, w := range tt.wrong {
+						is.Equal(slices.Compare([]int{w}, []int{got}), 1)
+					}
+				}
+
+				is.True(!slices.Contains(tt.wrong, got))
+				return
+			}
+			is.Equal(got, tt.want)
+		})
+	}
+}
+
+func Test_partTwo(t *testing.T) {
+	tests := []struct {
+		name  string
+		lines []string
+		want  int
+	}{
+		{
+			name:  "check test input length",
+			lines: GetTestInput(),
+			want:  12,
+		},
+		{
+			name:  "check length",
+			lines: GetInput(),
+			want:  50,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			is := is.New(t)
+			got := partTwo(tt.lines)
+			is.Equal(got, tt.want)
+		})
+	}
+}
